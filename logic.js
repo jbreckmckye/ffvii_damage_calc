3e5fbf33e1dee6e87dc4d7603a80414ac3038c66
jbreckmckye <jimmy@breck-mckye.com>
2013-10-29 21:10:15 +0000

    Initial commit for files

---
+++ logic.js
@@ -0,0 +1,374 @@
+//Debugging controls
+var debug = {
+	state : true,
+	print : function(string) {
+		if (this.state == true) {
+			console.log(string);
+		}
+	},
+	enumerate : function(object, level) {
+	    level = level || "";
+	    for (var property in object) {
+	        var propertyType = (typeof object[property]);	        
+	        console.log(level + property + " : " + propertyType + " = " + object[property]);
+	        if (propertyType === "object") {
+	            recur(level, object[property]);
+	        }
+	        function recur(level, object) {
+	            level += "-- ";
+	            debug.enumerate(object, level);
+	        }
+	    };
+	}
+}
+
+debug.print("Debug is on");
+
+//Constants
+var constant = {
+	overflowValue : 2147483648,
+	rndOverflowLowerBound : 524288,
+	rndOverflowTotalThreshold : 559095
+}
+
+var calculator = (function() {
+// JavaScript singleton pattern. Returns object 'calculator' with public interfaces	
+		
+	// CALCULATOR PRIVATE MEMBERS
+	
+	// Constructor for 'damageObject' that carries a log of damage changes, flags for overflow etc.
+	function DamageObject() {
+		
+		// DAMAGEOBJECT PRIVATE MEMBERS
+		
+		var damageLog = [];
+		
+		var overflowFlag = false;		
+		
+		var healFlag = false;
+		
+		// DAMAGEOBJECT PUBLIC INTERFACES
+		
+		this.setDamage = function(newDamage,cause) {
+			damageLog.push([newDamage,cause]);
+		}
+		
+		this.multiplyDamage = function(multiplier,cause) { 			
+			damageLog.push([(this.getCurrentDamage() * multiplier),cause]);
+		}
+		
+		this.getCurrentDamage = function() {
+		    if (damageLog.length > 0) {
+		      return damageLog[damageLog.length - 1][0];
+			}
+			else {
+			  return [0, "No damage logged"];
+			}
+		}
+		
+		this.getDamageTrace = function() {
+			return damageLog;
+		}
+		
+		this.setOverflowFlag = function() {
+			overflowFlag = true;
+		}
+		
+		this.getOverflowFlag = function() {
+			return overflowFlag;
+		}
+		
+		this.getRandomCappedDamage = function() {
+			var dmg = this.getCurrentDamage();
+			debug.print("Random variation start: " + dmg);
+			
+			var max_i = dmg * (3841 + 255);
+			var max_ii = max_i / 4096; 
+			
+			if (max_i > constant.overflowValue) {			
+				this.setOverflowFlag();
+			}
+			
+			var min_i = dmg * (3841 + 1);
+			var min_ii = min_i / 4096;
+			
+			// TODO - REFACTOR THIS CAPPING / SCOPING INTO SOMETHING NICER
+			
+			if (max_ii > 9999) {
+			    max_ii = 9999;
+			}
+			
+			if (min_ii > 9999) {
+			    min_ii = 9999;
+			}
+			
+			if (max_ii < 1) {
+			    max_ii = 1;
+			}
+			
+			if (min_ii < 1) {
+			    min_ii = 1;
+			}
+			
+			return {
+				maximum : max_ii,
+				minimum : min_ii
+			}
+		}
+	} // END DAMAGEOBJECT CONSTRUCTOR
+	
+	
+	function xCritical(damageObject) {	    
+		damageObject.multiplyDamage(2,"Critical hit modifier (x2)");
+	} 	
+ 	  
+    function xBerserk(damageObject) {
+        damageObject.multiplyDamage(1.5,"Berserk multiplier (x1.5)");
+    } 	
+ 	
+	function xBackRow(damageObject){
+	    damageObject.multiplyDamage(0.5,"Close range attack over distance (1/2)");
+	}
+	
+	function xDefend(damageObject) {
+	    damageObject.multiplyDamage(0.5,"Defending (1/2)")
+	}
+	
+    function xBackAttack(damageObject, defenderVulnerability) {
+        damageObject.multiplyDamage(defenderVulnerability, "Back attacked (x" + defenderVulnerability + ")");
+    }
+    
+    function xFrog(damageObject) {
+        damageObject.multiplyDamage(0.25,"Frog reduction (1/4)");
+    }   
+
+    function xSadness(damageObject) {
+        damageObject.multiplyDamage(0.7,"Sadness reduction (7/10)");
+    }
+    
+    function xQuadraPenalty(damageObject){
+        damageObject.multiplyDamage(0.5, "Damage halved per quadra-cast (1/2)");
+    }
+    
+    function xSplitDamage(damageObject){
+        damageObject.multiplyDamage((2/3),"Split damage for multi-tar (2/3)");
+    }
+    
+    function xBarrier(damageObject) {
+        damageObject.multiplyDamage(0.5,"Barrier/MBarrier applied (1/2)");
+    }
+	
+	function xMini(damageObject) {
+		damageObject.setDamage(0,"Minified (= 0)");
+	}
+	
+	function xFinalize(damageObject) {
+	    
+	    var finalDmg = damageObject.getCurrentDamage();
+	    var finalDmgMsg = "Final damage. ";
+	    
+	    
+	    if (finalDmg < 1) {
+	        finalDmgMsg += "Will round to 1. "
+	    }
+	    
+	    if (finalDmg > 9999) {
+	        finalDmgMsg += "Will cap at 9999. ";
+	    }
+	    
+	    if (finalDmg > constant.rndOverflowLowerBound && finalDmg < constant.rndOverflowTotalThreshold) {
+	        finalDmgMsg += "May OVERFLOW in randomization. "
+	    }
+	    
+	    if (finalDmg >= constant.rndOverflowTotalThreshold) {
+	        finalDmgMsg += "Will OVERFLOW in randomization. "
+	    }    
+
+		damageObject.setDamage(finalDmg, finalDmgMsg);
+		
+	}	
+	   
+    function xMultihit(damageObject, hits) {
+        damageObject.multiplyDamage(hits, "Multiple hits (x" + hits + ")")
+    }
+	
+	function base_physical(damageObject, scenario) {
+		var as = scenario.attackerStats; var ds = scenario.defenderStats; var skillPower = scenario.skillPower;
+
+        var totalAttack = as.attack + as.weapon;
+        
+        var initDamage = (skillPower / 16) * (totalAttack + (((as.level + totalAttack) / 32) * ((as.level * totalAttack) / 32)));
+		combinedPower = skillPower * (512 - ds.defence) * initDamage;
+			
+		baseDamage = combinedPower / (16 * 512);
+		
+		var overflowString = "";
+		if (combinedPower > constant.overflowValue) {
+			damageObject.setOverflowFlag();
+			overflowString = " - OVERFLOW";
+		}
+		
+		damageObject.setDamage(baseDamage,"Physical base damage" + overflowString);				
+		return damageObject;
+	}
+	
+	function base_magical(damageObject, scenario) {
+		var as = scenario.attackerStats; var ds = scenario.defenderStats; var skillPower = scenario.skillPower;
+		
+		attackerPower = 6 * (as.magic + as.level);
+		baseDamage = (skillPower * (512 - ds.mdef) * attackerPower) / (16 * 512);
+				
+		damageObject.setDamage(baseDamage,"Magical base damage");
+		return damageObject;
+	}
+	
+	function base_curative (attackerStats, skillPower, defenderStats) {
+		var as = attackerStats; var ds = defenderStats;
+		
+		attackerPower = 6 * (as.magic * as.level);
+		baseDamage = attackerPower + (22 * skillPower); 
+		
+		damageObject.setDamage(baseDamage,"Curative base damage");
+		return damageObject;		
+	}
+	
+	function applyModifiers(damageObject, modifiers) {
+	    var dmg = damageObject;
+	    
+	    
+	    if (modifiers.critical) {
+            xCritical(dmg);                             
+        }
+        
+        if (modifiers.berserk) {
+            xBerserk(dmg);
+        }
+        
+        if (modifiers.backRow) {
+            xBackRow(dmg);
+        }
+        
+        if (modifiers.defending) {
+            xDefend(dmg);
+        }
+        
+        if (modifiers.backAttacked) {
+            xBackAttack(dmg, modifiers.backAttackMultiplier);
+        }
+        
+        if (modifiers.frog) {
+            xFrog(dmg);
+        }
+               
+        if (modifiers.sadness) {
+            xSadness(dmg);
+        }
+        
+        if (modifiers.split) {
+            xSplitDamage(dmg);
+        }
+        
+        if (modifiers.quadra) {
+            xQuadraPenalty(dmg);
+        }
+        
+        if (modifiers.barriers) {
+            xBarrier(dmg);
+        }     
+        
+        if (modifiers.mini) {
+            xMini(dmg);
+        }
+        
+        if (modifiers.multiHit > 0) {
+            xMultihit(dmg, modifiers.multiHit);
+        }
+        
+        xFinalize(dmg);
+        
+        return dmg;
+        
+	}
+	
+	function getModifiersForPhysical(conditions) {
+	    modifiers = conditions;	    
+	    modifiers.quadra = false;
+	    
+	    return modifiers;	    
+	}
+	
+	function getModifiersForMagical(conditions) {
+	    modifiers = conditions;
+	    
+	    // Remove physical-only modifiers
+	    modifiers.mini = false;
+	    modifiers.frog = false;
+	    modifiers.backAttacked = false;
+	    modifiers.defending = false;
+	    modifiers.longRange = false;
+	    modifiers.berserk = false;
+	    modifiers.critical = false;
+	    
+	    return modifiers;
+	}
+
+	
+	// MEMBERS TO BE EXPOSED AS PUBLIC
+	
+	function physicalFormula(scenario) {
+	    
+	    var dmg = new DamageObject();
+        dmg = base_physical(dmg, scenario);
+        var modifiersToApply = getModifiersForPhysical(scenario.conditions);
+                
+        dmg = applyModifiers(dmg, modifiersToApply);
+        
+        return dmg;
+	
+	}
+	
+	function magicalFormula(scenario) {
+	    var dmg = new DamageObject();
+	    dmg = base_magical(dmg, scenario);
+	    var modifiersToApply = getModifiersForMagical(scenario.conditions);
+	    
+	    dmg = applyModifiers(dmg, modifiersToApply);
+	    
+	    return dmg;
+	
+	}
+		
+	return {
+		physicalFormula : physicalFormula,
+		magicalFormula : magicalFormula
+	};
+	
+})(); //end of iife that returns calculator object
+
+// TEST
+window.x = {
+        type : 'physical',
+        skillPower : 16,
+        attackerStats : {
+            level : 50,
+            attack : 78,
+            magic : 4,
+            weapon : 99,
+            heroDrinks : 0
+        },
+        defenderStats : {
+            defence : 120,
+            mdef : 1,
+            heroDrinks : 0,
+            dragonForce : 0
+        },
+        conditions : {
+            critical : true
+        }
+    };
+    
+window.c = calculator.physicalFormula;
+
+
+
+
---
+++ ui.js
@@ -0,0 +1,244 @@
+// GENERIC DOM EXTENSIONS
+
+function addListener(element, eventName, handler) {
+  if (element.addEventListener) {
+    element.addEventListener(eventName, handler, false);
+  }
+  else if (element.attachEvent) {
+    element.attachEvent('on' + eventName, handler);
+  }
+  else {
+    element['on' + eventName] = handler;
+  }
+}
+
+
+// UI HELPERS
+
+function hide(elementSet) {  
+    
+    for (index in elementSet) {
+        if (elementSet[index].setAttribute) {           
+            
+            elementSet[index].setAttribute('style', 'display: none');
+        }
+    }
+    return elementSet;  
+}
+
+function unhide(elementSet) {    
+    
+    for (index in elementSet) {
+        if (elementSet[index].removeAttribute) {
+            elementSet[index].removeAttribute('style');
+        }
+    }
+    return elementSet;  
+}
+
+
+
+
+
+// FORM CONTROL
+
+/*Constrain numeric inputs*/
+var inputs = document.getElementById('main').getElementsByTagName('input');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].onblur = function() { 
+        if (isNaN((Number(this.value))) || this.value < 0) {
+            this.value = this.defaultValue;
+        }
+
+        else if (this.value > 255) {
+            this.value = 255;
+        }
+    }
+}
+
+
+/*Set up calculate button*/
+var calculateButtons = document.getElementsByName('calculate');
+for (var i = 0; i < calculateButtons.length; i++) {
+    addListener(calculateButtons[i], 'click', runCalculator);
+}
+
+
+// REVEAL CONTENT
+
+
+function showResults(damageObject) {
+
+    function round(number) {
+        return Math.floor(number);
+    }    
+
+    function addBreakdown(damage, note) {
+        console.log(damage);
+        console.log(note);
+        
+        var target = document.getElementById('breakdown');
+        
+        var dt = document.createElement('dt');
+        dt.textContent = damage;
+        
+        var dd = document.createElement('dd');
+        dd.textContent = note;
+        
+        target.appendChild(dt);
+        target.appendChild(dd);    
+    }
+    
+    function clearResults() {
+        var targets = [];
+        targets.push(document.getElementById('breakdown'));
+        
+        for (var i = 0; i < targets.length; i++) {
+            targets[i].innerHTML = "";
+        }
+    }
+    
+    function postDamage(max, min) {
+        var maxSpan = document.getElementById('result-max');
+        var minSpan = document.getElementById('result-min');
+        
+        maxSpan.innerHTML = max;
+        minSpan.innerHTML = min;
+    }
+    
+    function postOverflowstate(overflowFlag) {
+        var target = document.getElementById('result-overflow');
+        var text = "not possible";
+        
+        if (overflowFlag === true) {
+            text = "possible";
+        }
+        
+        target.innerHTML = text; 
+    }
+    
+    // Prettify data
+    
+    var maxDamageDisplay = damageObject.getRandomCappedDamage().maximum;
+    maxDamageDisplay = round(maxDamageDisplay);
+    
+    var minDamageDisplay = damageObject.getRandomCappedDamage().minimum;
+    minDamageDisplay = round(minDamageDisplay);
+    
+        
+    // Clear existing data
+    clearResults();
+    
+    // Post new data
+    postDamage(maxDamageDisplay, minDamageDisplay);
+    postOverflowstate(damageObject.getOverflowFlag());
+    
+    log = damageObject.getDamageTrace();               
+    for (var i = 0; i < log.length; i++) {        
+        var logValue = round(log[i][0]);
+        var logNote = log[i][1];        
+        addBreakdown(logValue, logNote);            
+    }        
+    
+    //Show-hide panels
+    var initialState = document.getElementById('beforeSearch');
+    initialState.style.display = "none";
+    var postState = document.getElementById('afterSearch');
+    postState.style.display = 'block';
+        
+};
+
+function parseForm() {
+    
+    function getval(fieldID) {
+        
+        var value = undefined;        
+        var target = document.getElementById(fieldID);
+        
+        if (target.tagName === "SELECT") {            
+            value = target.options[target.selectedIndex].value;            
+        }        
+        else if (target.tagName === "INPUT" && target.type === "text") {
+            value = Math.floor(target.value);
+        }        
+        else if (target.tagName === "INPUT" && target.type === "radio") {
+            var radioSetName = target.name;
+            var radioSet = document.getElementsByName(radioSetName);
+            for (var i = 0; i < radioSet.length; i++) {
+                var thisRadio = radioSet[i];
+                if (thisRadio.checked) {
+                    value = thisRadio.value;
+                }              
+            }          
+        }
+        else if (target.tagName === "INPUT" && target.type === "checkbox") {
+            value = target.checked;
+        }
+             
+        else {
+            debug.print("Could not parse field - " + fieldID);
+        }
+
+        
+        return value;                
+    }
+    
+    var parsedData = {
+        type : getval('type'),
+        skillPower : getval('power'),
+        attackerStats : {
+            level : getval('level'),
+            attack : getval('strength'),
+            magic : getval('magic'),
+            weapon : getval('weapon'),
+            heroDrinks : getval('atkr-hero-drinks')
+        },
+        defenderStats : {
+            defence : getval('defence'),
+            mdef : getval('mdefence'),
+            heroDrinks : getval('def-hero-drinks'),
+            dragonForce : getval('def-dragon-forces')
+        },
+        conditions : {
+            quadra : getval('quadra-magic'),
+            split : getval('split'),
+            berserk : getval('berserk'),
+            mini : getval('mini'),
+            critical : getval('critical'),
+            frog : getval('frog'),
+            barrier : getval('barrier'),
+            sadness : getval('sadness'),
+            defending : getval('defending'),
+            backRow : getval('back-row'),
+            backAttacked : getval('back-attacked'),
+            backAttackMultiplier : getval('back-attack-mod')
+        }
+    };
+    
+    return parsedData;
+    
+}
+
+
+// CONTROLLER
+
+function runCalculator() {
+    var data = parseForm();
+    
+    var result;
+    if (data.type == "Physical") {
+        result = calculator.physicalFormula(data);
+    }
+    else if (data.type == "Magical") {
+        result = calculator.magicalFormula(data);
+    }
+    
+    showResults(result);
+}
+
+
+
+
+
+
+
